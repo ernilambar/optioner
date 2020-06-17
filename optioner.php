2675ccf4b7ede469a15e70d393b95e703a316969
Nilambar Sharma <nilambar@outlook.com>
2020-06-17 21:27:44 +0545

    Add plugin file

---
+++ optioner.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Optioner
+ * Description: Minimal option framework.
+ * Version: 1.0.0
+ * Author: Nilambar Sharma
+ * Author URI: https://www.nilambar.net
+ * Text Domain: optioner
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package Optioner
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'OPTIONER_BASENAME', basename( dirname( __FILE__ ) ) );
+define( 'OPTIONER_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) );
+define( 'OPTIONER_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
